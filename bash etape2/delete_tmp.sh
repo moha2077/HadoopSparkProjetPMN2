3f297140e464f465dfc08f9d3ea2ac08b909ee2d
MohamedBoudjenane <mohaboudjenane@gmail.com>
2023-07-31 02:43:59 +0200

    bash

--- bash etape2/delete_tmp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm -r C:\Users\Ordi\Documents\GitHub\HadoopSparkProjetPMN\bash etape2\delete_tmp.sh\data_tmp
---
+++ bash_etape2/delete_tmp.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -r C:\Users\Ordi\Documents\GitHub\HadoopSparkProjetPMN\bash_etape2\delete_tmp.sh\data_tmp
